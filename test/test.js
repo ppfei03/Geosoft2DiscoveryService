e23849ef32d6abc50b575629d143001b641085c9
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-15 16:20:20 +0200

    Setup testing.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('Mocha testing is working', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
