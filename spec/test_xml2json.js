ce00a7d61bdf919bdd16ec75f852f3a9398d67a5
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-15 23:14:08 +0200

    Introducing jasmine tests.

---
+++ spec/test_xml2json.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const xml2json = require('../cache/metadataCache.js').xml2json;
+
+
+
+
+describe('xml2json is basically working', () => {
+  it('transforms the Hello xml2js into the expected string', () => {
+    const hello = "<root>Hello xml2js!</root>"
+    xml2json(hello).then(res => {
+      assert.deepEqual(res, {
+        root: 'Hello xml2js!'
+      });
+    });
+  });
+});
