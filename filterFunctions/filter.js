5b54810c03a5e6154bff4a31863674ca1d1aebd2
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-10-15 23:13:43 +0200

    Starting implementing filter functionality.

---
+++ filterFunctions/filter.js
@@ -0,0 +1,13 @@
+// Main file. Aggregates the filter functionalities
+
+// // Template
+// const input = {
+//   arrayOfObjects: []
+//   filterObject: {}
+// };
+//
+// // Template
+// const filterObject : {
+//   minDateTime : String,
+//   
+// }
