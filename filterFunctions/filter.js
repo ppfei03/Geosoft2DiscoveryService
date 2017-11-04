26d9622aa2f7282c4b2caee76792bf642daafa5a
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-11-04 11:01:18 +0100

    min Date(Time) filter implemented

--- filterFunctions/filter.js
+++ filterFunctions/filter.js
@@ -1,13 +1,26 @@
 // Main file. Aggregates the filter functionalities
+// promObj is passed through the filters. It is: {filterResult: copyOfCache, query: req.query}
 
-// // Template
-// const input = {
-//   arrayOfObjects: []
-//   filterObject: {}
-// };
-//
-// // Template
-// const filterObject : {
-//   minDateTime : String,
-//   
-// }
+const minDateFilter = require('./minDateFilter.js').minDateFilter;
+const metadataCache = require('../cache/metadataCache');
+
+
+function filter(req) {
+  return new Promise((resolve, reject) => {
+    try {
+
+      let promObj = {filterResult: null, query: null};
+
+      promObj.filterResult = JSON.parse( JSON.stringify(metadataCache.getCache())  );
+      promObj.query = req.query;
+      minDateFilter(promObj).then(res => {resolve(res)});
+    }
+    catch(error) {
+      resolve(error)
+    }
+  });
+};
+
+module.exports = {
+  filter: filter
+}
