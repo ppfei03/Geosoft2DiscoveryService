8ba6aed6afabec75f3826997058f10dda0cfec8f
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-11-06 22:09:18 +0100

    removing dead endpoint

--- routes.js
+++ routes.js
@@ -19,13 +19,22 @@ const metadataCache = require('./cache/metadataCache');
  * ?maxDateTime: how old the oldes image is allowed to be (inclusive)
  * @type {JSON}
  */
-router.get("/search", (req, res, next) => {
-  console.log('info', 'Request at route /search', req.query);
+// router.get("/search", (req, res, next) => {
+//   console.log('info', 'Request at route /search', req.query);
+//
+//   res.send('/search')
+// });
 
-  res.send('/search')
-});
 
-
+/**
+ * Endpoint for searching
+ * Processed query parameters are:
+ * ?searchString: Commaseperated String of elements that will be used for matching (matching what?)
+ * ?bbox: Spatial extent as bbox in WGS84: minx, miny, maxx, maxy
+ * ?minDateTime: how old the younges image is allowd to be (inclusive)
+ * ?maxDateTime: how old the oldes image is allowed to be (inclusive)
+ * @type {JSON}
+ */
 // Endpoint for development, returning the whole cache
 router.get("/datasets", (req, res, next) => {
 
