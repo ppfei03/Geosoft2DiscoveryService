2b4c6ee9c3b8929456ea110b5b8b23d4513b90a5
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-12-18 11:44:00 +0100

    Update path to folder

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use('/img', express.static(path.join(__dirname, '/../sentinel2')));
+app.use('/img', express.static(path.join(__dirname, '/../sentinel2_0_8500Backup')));
 
 app.use(routes);
 
