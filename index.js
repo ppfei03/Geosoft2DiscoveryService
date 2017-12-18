d6e447f6f8fea240a9d9f0c17c8a631db68d1b4e
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-12-18 11:54:52 +0100

    Trying to fix cors problem

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const metadataCache = require('./cache/metadataCache');
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(cors);
+app.use(cors());
 app.use(expressLogging(logger));
 app.use(compression());
 app.use(bodyParser.urlencoded({
