3ddb25ea62d3f18e5e22b1069707717b28f52ad0
Trzaska <Niklas.Trzaska@uni-muenster.de>
2018-01-08 21:59:53 +0100

    Adding url of this server

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 // Change in this file the url to the folder, where your preprocessed Sentinel 2 data is.
 const config = {
-    "urlToDataFolder" : "./../sentinel2_0_8500Backup"
+    "urlToDataFolder" : "./../sentinel2_0_8500Backup",
+    "urlOfThisService" : "http://gis-bigdata:11016"
 }
 
 module.exports = {
