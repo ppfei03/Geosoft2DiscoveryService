1984dca4df146c1d699d4f3ae911b9e6ba8fd6fa
Trzaska <Niklas.Trzaska@uni-muenster.de>
2018-01-08 22:57:18 +0100

    Removing not needed folder for tests

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('Mocha testing is working', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
