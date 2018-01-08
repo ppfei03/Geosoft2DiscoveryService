c92f05604c4b26f5c8f1e04ba72d075b49d03fcb
Trzaska <Niklas.Trzaska@uni-muenster.de>
2018-01-08 23:03:10 +0100

    Refactoring

--- routes.js
+++ routes.js
@@ -1,30 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const logger = require('logops');
 const filter = require('./filterFunctions/filter.js').filter;
 const pixelValue = require('./pixelValue/pixelValue.js').pixelValue;
-
-
-
-// For development:
-const metadataCache = require('./cache/metadataCache');
-
-
-
-/**
- * Endpoint for searching
- * Processed query parameters are:
- * ?searchString: Commaseperated String of elements that will be used for matching (matching what?)
- * ?bbox: Spatial extent as bbox in WGS84: minx, miny, maxx, maxy
- * ?minDateTime: how old the younges image is allowd to be (inclusive)
- * ?maxDateTime: how old the oldes image is allowed to be (inclusive)
- * @type {JSON}
- */
-// router.get("/search", (req, res, next) => {
-//   console.log('info', 'Request at route /search', req.query);
-//
-//   res.send('/search')
-// });
 
 
 /**
@@ -36,32 +13,29 @@ const metadataCache = require('./cache/metadataCache');
  * ?maxDateTime: how old the oldes image is allowed to be (inclusive)
  * @type {JSON}
  */
-// Endpoint for development, returning the whole cache
 router.get("/datasets", (req, res, next) => {
-
-    logger.info(req.query);
-
-      filter(req).then(filteredCache => {res.send(filteredCache.filterResult)}).catch(err => {res.send({status: 'error', error: err})})
-      // console.log('info', 'Request at route /getCache', req.query);
-      // res.send(metadataCache.getCache());
-
+    filter(req).then(filteredCache => {
+        res.send(filteredCache.filterResult)
+    }).catch(err => {
+        res.send({status: 'error', error: err})
+    })
 });
 
-// // /img?identifier=&resolution=&band=&z=&x&y
-// router.get("/img", (req, res, next) => {
-//
-//     logger.info(req.query);
-//     let promObj = {req: req};
-//
-//     getTile(promObj).then(promObj => {logger.info('gotPath'); logger.info(promObj.path); logger.info(path.join(__dirname, promObj.path)); res.sendFile(path.join(__dirname, promObj.path))}).catch(error => {res.send({status: 'error', error: error})});
-//
-// });
 
-//Endpoint for MSIL1C
-//expect query parameters identifier and band, lat, long
+/**
+ *Returns the original pixel value of a requested image.
+ * Needed reques parameters are:
+ * ?identifier
+ * ?lat
+ * ?long
+ */
 router.get("/pixelValue", (req, res, next) => {
     const reqObj = {req: req};
-    pixelValue(reqObj).then(resp => {res.send(resp)}).catch(error => {res.send({status: 'error', error: error})});
+    pixelValue(reqObj).then(resp => {
+        res.send(resp)
+    }).catch(error => {
+        res.send({status: 'error', error: error})
+    });
 });
 
 module.exports = router;
