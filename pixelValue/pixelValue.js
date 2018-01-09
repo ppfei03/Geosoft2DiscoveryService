8723e69fd69c72733d45f1a8ac1ba1fb5abcb015
Trzaska <Niklas.Trzaska@uni-muenster.de>
2018-01-09 21:15:32 +0100

    Dummy values as work around for broken function

--- pixelValue/pixelValue.js
+++ pixelValue/pixelValue.js
@@ -174,7 +174,76 @@ function getGdallocationinfoAsJson(promObj) {
         try {
 
 
-            resolve({"John": 42})
+            // Dummy response due to problems (see below),
+            resolve({
+                "Report":{
+                    "pixel":"8496",
+                    "line":"4074",
+                    "BandReport":[
+                        {
+                            "band":"1",
+                            "Value":"161"
+                        },
+                        {
+                            "band":"2",
+                            "Value":"129"
+                        },
+                        {
+                            "band":"3",
+                            "Value":"115"
+                        }
+                    ]
+                }
+            });
+
+
+            /*
+For unknown reasons, this function stopped working properly, all of the sudden.
+It was detected on the 08.01.2017.
+
+Some (not all - it is not predictable) requests are processed correctly. Then the main process is blocked
+
+**********
+error
+{ Error: Command failed: gdallocationinfo -wgs84 -xml ../sentinel2/S2A_MSIL1C_20171010T104021_N0205_R008_T30PTB_20171010T104947.SAFE/IMG_DATA/T30PTB_20171010T104021_TCI.jp2 -5 15
+
+    at ChildProcess.exithandler (child_process.js:272:12)
+    at ChildProcess.emit (events.js:159:13)
+    at maybeClose (internal/child_process.js:943:16)
+    at Socket.stream.socket.on (internal/child_process.js:363:11)
+    at Socket.emit (events.js:159:13)
+    at Socket.ReaddirReadable.destroy (/home/n_trza01/Geosoft2DiscoveryService/node_modules/fs-readdir/index.js:159:8)
+    at Socket.onSocketFinish (net.js:279:17)
+    at Socket.emit (events.js:164:20)
+    at finishMaybe (_stream_writable.js:605:14)
+    at endWritable (_stream_writable.js:613:3)
+  killed: false,
+  code: null,
+  signal: null,
+  cmd: 'gdallocationinfo -wgs84 -xml ../sentinel2/S2A_MSIL1C_20171010T104021_N0205_R008_T30PTB_20171010T104947.SAFE/IMG_DATA/T30PTB_20171010T104021_TCI.jp2 -5 15' }
+**********
+data
+<Report pixel="8496" line="4074">
+  <BandReport band="1">
+    <Value>161</Value>
+  </BandReport>
+  <BandReport band="2">
+    <Value>129</Value>
+  </BandReport>
+  <BandReport band="3">
+    <Value>115</Value>
+  </BandReport>
+</Report>
+
+**********
+
+But as one can see, the data is still there, but the server crashed.
+
+Other solutions with native child processes in node, and with shelljs as an alternative to node-cmd has been tested, but the results were similar or the
+needed functionality was not supported.
+
+
+ */
 
    /*         const command = 'gdallocationinfo -wgs84 -xml ' + promObj.url + ' ' + promObj.req.query.long + ' ' + promObj.req.query.lat;
             // 'gdallocationinfo -wgs84 -xml T30PTB_20171010T104021_TCI.jp2 -5 15',
